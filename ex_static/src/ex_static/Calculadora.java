92f652dc3dc6206ead8001620d0ff5cc251a216e
Edson <120113548+WillCossBr@users.noreply.github.com>
2024-03-28 21:31:12 -0300

    Add files via upload

---
+++ ex_static/src/ex_static/Calculadora.java
@@ -0,0 +1,51 @@
+package ex_static;
+
+public class Calculadora
+{
+
+	public static void soma(float a, float b)
+	{
+		float resultado;
+		float som1;
+		float som2;
+		som1 = a;
+		som2 = b;
+		resultado = som1 + som2;
+		System.out.println(resultado);
+	}
+	
+	public static void subtracao(float a, float b)
+	{
+		float resultado;
+		float sub1;
+		float sub2;
+		sub1 = a;
+		sub2 = b;
+		resultado = sub1 - sub2;
+		System.out.println(resultado);
+	}
+	
+	public static void multiplicacao(float a, float b)
+	{
+		float resultado;
+		float mul1;
+		float mul2;
+		mul1 = a;
+		mul2 = b;
+		resultado = mul1 * mul2;
+		System.out.println(resultado);
+	}
+	
+	public static void divisao(float a, float b)
+	{
+		float resultado;
+		float div1;
+		float div2;
+		div1 = a;
+		div2 = b;
+		resultado = div1 / div2;
+		System.out.println(resultado);
+	}
+
+}
+
---
+++ ex_static/src/ex_static/Principal.java
@@ -0,0 +1,69 @@
+package ex_static;
+
+import java.util.Scanner;
+
+public class Principal
+{
+
+	public static void main(String[] args)
+	{
+		int menu;
+		float num1;
+		float num2;
+		
+		try (Scanner teclado = new Scanner(System.in))
+		{
+		
+			System.out.println("CALCULADORA:             ");
+			System.out.println("1 - SOMA                 ");
+			System.out.println("2 - SUBTRAÇÃO            ");
+			System.out.println("3 - MULTIPLICAÇÃO        ");
+			System.out.println("4 - DIVISÃO              ");
+			System.out.println("DIGITE A OPÇÃO DESEJADA: ");
+		
+			menu = teclado.nextInt();
+		
+			switch(menu)
+			{
+			case 1:
+				System.out.println("Digite um número: ");
+				num1 = teclado.nextFloat();
+				System.out.println("Digite um número: ");
+				num2 = teclado.nextFloat();
+				Calculadora.soma(num1, num2);
+			break;
+			
+			case 2:
+				System.out.println("Digite um número: ");
+				num1 = teclado.nextFloat();
+				System.out.println("Digite um número: ");
+				num2 = teclado.nextFloat();
+				Calculadora.subtracao(num1, num2);
+			break;
+			
+			case 3:
+				System.out.println("Digite um número: ");
+				num1 = teclado.nextFloat();
+				System.out.println("Digite um número: ");
+				num2 = teclado.nextFloat();
+				Calculadora.multiplicacao(num1, num2);
+			break;
+			
+			case 4:
+				System.out.println("Digite um número: ");
+				num1 = teclado.nextFloat();
+				System.out.println("Digite um número: ");
+				num2 = teclado.nextFloat();
+				Calculadora.divisao(num1, num2);
+			break;
+			
+			default:
+				System.out.println("Opção de menu inválido!");
+			break;
+			}
+			
+		}
+		
+	}
+
+}
